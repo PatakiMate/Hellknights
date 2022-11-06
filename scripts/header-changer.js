927e411b396a90a5c70fd10d8bd1b4b056da1142
MatePataki <pataki.mate2003@gmail.com>
2022-11-06 13:25:55 +0100

    changed to tailwindcss

--- scripts/header-changer.js
+++
@@ -1,7 +0,0 @@
-let width = window.innerWidth;
-console.log("width: " + width);
-if(width <= 1199.98) {
-document.getElementById("header-top").className = 'container';
-} else {
-    document.getElementById("header-top").className = 'container-fluid'; 
-}
